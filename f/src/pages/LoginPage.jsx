3fa5e46011ebad9a35a4a148f1df30e7df354a44
Aravindhan109 <aravindhan1092004@gmail.com>
2025-07-28 09:56:18 +0530

    Update LoginPage.jsx

--- f/src/pages/LoginPage.jsx
+++ f/src/pages/LoginPage.jsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://ak-quizz-app-b.onrender.com/api/auth/login", {
         email,
         password,
       });
